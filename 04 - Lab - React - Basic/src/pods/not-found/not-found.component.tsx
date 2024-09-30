97739c8c252f56679529560eacba297437d03005
oleojake <oleojake@gmail.com>
2024-09-30 20:59:33 +0200

    not-found scene

--- 04 - Lab - React - Basic/src/pods/not-found/not-found.component.tsx
+++ 04 - Lab - React - Basic/src/pods/not-found/not-found.component.tsx
@@ -1,12 +1,27 @@
+import { routes } from "@/router";
+import { Box, Button, Typography } from "@mui/material";
 import React from "react";
 import { Link } from "react-router-dom";
+import classes from "./not-found.component.module.scss";
 
 export const NotFoundComponent: React.FC = () => {
 	return (
-		<>
-			<h2>404 - Page Not Found</h2>
-			<p>Lo sentimos, la página que estás buscando no existe.</p>
-			<Link to="/">Volver al inicio</Link>
-		</>
+		<Box className={classes.container}>
+			<Typography variant="h5" component="h2" className={classes.title}>
+				404 - Page Not Found
+			</Typography>
+			<Typography variant="body1" component="p">
+				Sorry, the page you are looking for does not exist.
+			</Typography>
+			<Button
+				className={classes.button}
+				component={Link}
+				to={routes.root}
+				variant="contained"
+				color="primary"
+			>
+				Back to list page
+			</Button>
+		</Box>
 	);
 };
