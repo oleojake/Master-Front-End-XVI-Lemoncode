b92843e257eece893daa9a6b8f2adde31e6d83ee
oleojake <oleojake@gmail.com>
2024-10-14 12:24:45 +0200

    scroll on swipeable drawer

--- 04_Lab_React_Context/src/pods/cart/components/swipeable-drawer.component.tsx
+++ 04_Lab_React_Context/src/pods/cart/components/swipeable-drawer.component.tsx
@@ -1,51 +1,22 @@
 import * as React from "react";
 import { Global } from "@emotion/react";
-import { styled } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-import { grey } from "@mui/material/colors";
 import Box from "@mui/material/Box";
 import SwipeableDrawer from "@mui/material/SwipeableDrawer";
 import { ProductList, ShoppingBasketIconComponent } from "./components/";
+import styles from "./swipeable-drawer.component.module.scss";
 
 const drawerBleeding = 56;
 
-const Root = styled("div")(({ theme }) => ({
-	height: "100%",
-	backgroundColor: grey[100],
-	...theme.applyStyles("dark", {
-		backgroundColor: theme.palette.background.default,
-	}),
-}));
-
-const StyledBox = styled("div")(({ theme }) => ({
-	backgroundColor: "#fff",
-	...theme.applyStyles("dark", {
-		backgroundColor: grey[800],
-	}),
-}));
-
-const Puller = styled("div")(({ theme }) => ({
-	width: 30,
-	height: 6,
-	backgroundColor: grey[300],
-	borderRadius: 3,
-	position: "absolute",
-	top: 8,
-	left: "calc(50% - 15px)",
-	...theme.applyStyles("dark", {
-		backgroundColor: grey[900],
-	}),
-}));
-
 export const SwipeableEdgeDrawer: React.FC = () => {
-	const [open, setOpen] = React.useState(false);
+	const [open, setOpen] = React.useState(true);
 
 	const toggleDrawer = (newOpen: boolean) => () => {
 		setOpen(newOpen);
 	};
 
 	return (
-		<Root>
+		<div className={styles.root}>
 			<CssBaseline />
 			<Global
 				styles={{
@@ -66,8 +37,9 @@ export const SwipeableEdgeDrawer: React.FC = () => {
 					keepMounted: true,
 				}}
 			>
-				<StyledBox
-					sx={{
+				<div
+					className={styles.styledBox}
+					style={{
 						position: "absolute",
 						top: -drawerBleeding,
 						borderTopLeftRadius: 8,
@@ -77,13 +49,15 @@ export const SwipeableEdgeDrawer: React.FC = () => {
 						left: 0,
 					}}
 				>
-					<Puller />
+					<div className={styles.puller} />
 					<Box sx={{ p: 2 }}>
 						<ShoppingBasketIconComponent />
 					</Box>
-				</StyledBox>
-				<ProductList />
+				</div>
+				<div className={styles.scrollableProductList}>
+					<ProductList />
+				</div>
 			</SwipeableDrawer>
-		</Root>
+		</div>
 	);
 };
